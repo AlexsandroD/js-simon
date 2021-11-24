51417b3e8e53942c47d96e3b12c5f36df6f72c44
Alex <adrdra88@gmail.com>
2021-11-24 17:03:22 +0100

    confronto

--- js/script.js
+++ js/script.js
@@ -1,26 +1,47 @@
-const time = setTimeout(countDown, 30000);
-
-function countDown(){
-    alert('hello');
-}
-
+const numeri = [];
+const tentativi =[];
 
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-
-const numeri = [];
 getRandomNumber(numeri);
 function getRandomNumber (array){
     for(let i = 0; i < 5; i++){
         array.push(random(1,100));
     }
 }
+creaNumeri(numeri);
+function creaNumeri(arrayNumeri){
+    
+    const numberContainer = document.createElement('div');
+    numberContainer.setAttribute('id', 'numeri');
+    
+    document.querySelector('.container').append(numberContainer);
+    
+    numberContainer.innerHTML = arrayNumeri;
+}
 
-const numberContainer = document.createElement('div');
-numberContainer.setAttribute('id', 'numeri');
-console.log(numberContainer)
+const time = setTimeout(countDown, 3000);
+function countDown(){
+    let count = 0;
+    for(let k = 0; k < 2; k++){
+        const guess = parseInt(prompt
+        ('inserisci un valore'));
 
-document.querySelector('.container').append(numberContainer);
+        tentativi.push(guess);
 
-numberContainer.innerHTML = numeri;
+        for(let j = 0; j < tentativi.length; j++){
+            if(tentativi.includes(numeri[k])){
+                console.log('ciao')
+            }
+        }
+    
+    }
+}
+
+
+
+
+
+
+
