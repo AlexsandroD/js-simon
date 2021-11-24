7bdb9109303aeb7ae683b6ca980d360004c083af
Alex <adrdra88@gmail.com>
2021-11-24 18:26:22 +0100

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -5,43 +5,56 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 getRandomNumber(numeri);
+
 function getRandomNumber (array){
     for(let i = 0; i < 5; i++){
         array.push(random(1,100));
     }
 }
+
+
+console.log(numeri);
+
 creaNumeri(numeri);
 function creaNumeri(arrayNumeri){
     
     const numberContainer = document.createElement('div');
     numberContainer.setAttribute('id', 'numeri');
     
-    document.querySelector('.container').append(numberContainer);
+    document.querySelector('.container-numeri').append(numberContainer);
+
     
     numberContainer.innerHTML = arrayNumeri;
 }
 
 const time = setTimeout(countDown, 3000);
+
 function countDown(){
+  
     let count = 0;
-    for(let k = 0; k < 2; k++){
+    for(let k = 0; k < 1; k++){
         const guess = parseInt(prompt
         ('inserisci un valore'));
 
         tentativi.push(guess);
 
-        for(let j = 0; j < tentativi.length; j++){
-            if(tentativi.includes(numeri[k])){
-                console.log('ciao')
-            }
-        }
-    
+       if(numeri.includes(guess)){
+          ++count;
+       }
     }
+    if(count == numeri.length){
+        document.querySelector('.vinto-perso').innerHTML = `<h2>Hai vinto! ${count}/5</h2>`;
+    }else{
+            document.querySelector('.vinto-perso').innerHTML = `<h2>Hai perso! ${count}/5</h2>`;
+    }
+    
 }
 
+const change = setTimeout(changeClass, 2900);
 
-
-
+function changeClass(){
+    document.querySelector('.container-numeri').classList.add('white');
+}
 
 
 
